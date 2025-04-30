b97c3e2cfd46772b61ae1dab63020ad71881546a
fandy <fandy@fandys-MacBook-Air.local>
2025-04-30 16:57:30 +0800

    tes

--- apps/nest-app/src/app/app.module.ts
+++ apps/nest-app/src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { CarModule } from '@nx-mono-test/nest-lib';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(
-      'mongodb+srv://fandy:fandygantengbanget@fandy.dhfot1z.mongodb.net/Development'
-    ),
-    CarModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CarModule],
   controllers: [AppController],
   providers: [AppService],
 })
